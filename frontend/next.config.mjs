6c031038947d24522fd4edfd2ed8e86f9e82ef8e
mightychid <ibrahimbashirilyasu@gmail.com>
2025-05-05 11:47:34 +0000

    user management

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,15 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-      }
-    ]
-  }
-}
-
 export default nextConfig;
